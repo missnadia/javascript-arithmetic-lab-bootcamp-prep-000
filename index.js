26236110994c8c6f3f31d870331d52be4898c072
Nadia Lee <miss.nadia.lee@gmail.com>
2017-09-25 22:29:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var x = 4;
+var y = 2;
+
+function add(x, y) {
+  return x + y;
+}
+function subtract(x, y) {
+  return x - y;
+}
+function multiply(x, y) {
+  return x * y;
+}
+function divide(x, y) {
+  return x / y;
+}
+function inc(x) {
+  return x += 1;
+}
+function dec(x) {
+  return x -= 1;
+}
+function makeInt(x) {
+  parseInt(x, 10);
+}
+
