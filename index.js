170fea40493eb6d77221e190c96e42dd01f4c971
Nadia Lee <miss.nadia.lee@gmail.com>
2017-09-25 22:53:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function makeInt(x) {
 }
 function isNaN(a) {
   var a = 'nonsense!';
-  parseInt(a, 10);
-  return ;
+  parseFloat(a);
+  return a;
 }
 function preserveDecimal(z) {
   var z = '0.456';
